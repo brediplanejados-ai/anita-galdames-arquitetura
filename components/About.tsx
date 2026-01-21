9ad0446d52222a271719e4a7b59cbbc277d1fd52
Bredi <contato@bredi.com.br>
2026-01-21 07:33:22 -0300

    Update: Profile image in About section

--- components/About.tsx
+++ components/About.tsx
@@ -9,7 +9,7 @@ const About: React.FC = () => {
                     <div className="relative group">
                         <div className="aspect-[4/5] rounded-[3rem] overflow-hidden shadow-2xl">
                             <img
-                                src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?auto=format&fit=crop&q=80&w=800"
+                                src="https://i.ibb.co/yFb4CzBk/image.png"
                                 alt="Anita Galdames"
                                 className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-105"
                             />
